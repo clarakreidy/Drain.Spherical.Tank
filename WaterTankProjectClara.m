e80499240c67293dd51b2a2c557a43c6a333ff19
paul30798 <paultohme98@gmail.com>
2021-05-15 13:16:56 +0300

    adding euler to graph

--- WaterTankProjectClara.m
+++ WaterTankProjectClara.m
@@ -1,4 +1,3 @@
-pkg load symbolic;
 clear all; close all; clc;
 
 fprintf("This program shows us the behavior of the water in a spherical water tank in the event of a leakage\n");
@@ -12,9 +11,8 @@ rHole=input("Give the radius of the hole at the bottom of the tank in meters\n")
 g=9.81;
 t0=0;
 step=1;
-
 time=0:step:tFinal;
-
+dim=(tFinal-t0)/step;
 %Water level should be containable in the tank
 if hWater>2*rTank
     error("Water volume is greater than tank volume...");
@@ -27,6 +25,27 @@ end
 
 %equation representing dh/dt
 dhdt = @(t,h) -1*(rHole^2*sqrt(2*g*h))/(2*h*rTank-h^2);
-y = ode23(dhdt, time, hWater);
+y1 = ode23(dhdt, time, hWater);
+y2=Euler(dhdt,t0,tFinal,hWater,step);
 
-plot(y.x, y.y)
+plot(y1.x, y1.y,' ro-- ',time,y2,' k ','linewidth',1);
+title("ODE23 vs Euler Method");
+xlabel('Time t in seconds');
+ylabel('H(t) in m');
+legend('ODE23, Euler');
+ylim([0 inf]);
+
+counter=1;
+fprintf("\nUsing Euler's Method:\n");
+while counter<=dim+1 && y2(counter)>0
+    fprintf("at %f ",time(counter));
+    fprintf("seconds we have %f ",y2(counter));
+    fprintf("m of water remaining in the tank\n");
+    counter=counter+1;
+end
+
+n=length(time);
+integrateH=@(h) (h^2-2*rTank*h)/(sqrt(h));
+integral=GaussianQ2(integrateH,hWater,0.001);
+timeToEmpty=integral/(rHole^2*sqrt(2*g));
+fprintf("Time needed to empty the tank is: %f seconds\n",timeToEmpty);
